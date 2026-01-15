a1211778631d3b2eddc245045f6ee9375222ec60
Diego <diegocandido310@gmail.com>
2026-01-15 15:41:17 -0300

    corrige acesso ao st.secrets para supabase

--- auth.py
+++ auth.py
@@ -2,8 +2,8 @@ import streamlit as st
 import os 
 from supabase import create_client
 
-SUPABASE_URL = st.secrets("SUPABASE_URL")
-SUPABASE_KEY = st.secrets("SUPABASE_KEY")
+SUPABASE_URL = st.secrets["SUPABASE_URL"]
+SUPABASE_KEY = st.secrets["SUPABASE_KEY"]
 
 supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
 
