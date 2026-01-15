b1bfd84953c31955f4da5dc0450f0028968de748
Diego <diegocandido310@gmail.com>
2026-01-15 18:13:23 -0300

    integração com supabase

--- app.py
+++ app.py
@@ -8,6 +8,23 @@ SUPABASE_KEY = st.secrets["SUPABASE_KEY"]
 
 supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
 
+def salvar_ganho(data, descricao, valor):
+    user = supabase.auth.get_user()
+    supabase.table("ganhos").insert({
+        "user_id": user.user.id,
+        "data": data.isoformat(),
+        "descricao": descricao,
+        "valor": valor
+    }).execute()
+
+def salvar_despesa(data, descricao, valor):
+    user = supabase.auth.get_user()
+    supabase.table("despesas").insert({
+        "user_id": user.user.id,
+        "data": data.isoformat(),
+        "descricao": descricao,
+        "valor": valor
+    }).execute()
 
 st.set_page_config(
     page_title= "Calculadora financeira",
@@ -53,15 +70,47 @@ def mostrar_app():
     with tab_ganho:
         st.subheader("Registrar Ganho")
 
+        data = st.date_input("Data", key = "data_ganho")
+        descricao = st.text.input("Descrição", key = "desc_ganho")
+        valor = st.number_input("valor", min_value=0.0, format="%.2f", key = "valor_ganho")
+
+        if st.button("Salvar ganho"):
+            if descricao.strip() == "" or valor <0:
+                st.warning("Preencha todos os campos corretamente")
+            
+            else:
+                salvar_ganho(data, descricao, valor)
+                st.success("Ganho registrado com sucesso!")
                 
 
     with tab_despesa:
-        st.subheader("Registrar despesa")
+        st.subheader("Registrar Despesa")
+
+        data = st.date_input("Data", key="data_despesa")
+        descricao = st.text.input("Descrição", "desc_despesa")
+        valor = st.number_input("valor", min_value=0.0, format="%.2f", key= "valor_despesa")
+
+        if st.button("Salvar ganho"):
+            if descricao.strip() == "" or valor <0:
+                st.warning("Preencha todos os campos corretamente")
+            
+            else:
+                salvar_ganho(data, descricao, valor)
+                st.success("Ganho registrado com sucesso!")
        
 
     with tab_resumo:
-        st.subheader("resumo")
+        st.subheader("Resumo")
 
+        ganhos = supabase.table("ganhos"). select("*").execute().data
+        despesas = supabase.table("despesas").select("*").execute().data
+
+        total_ganhos = sum(item["valor"] for item in ganhos)
+        total_despesas = sum(item["valor"] for item in despesas)
+
+        st.metric("Total de ganhos", f"R$ {total_ganhos:.2f}")
+        st.metric("Total de despesas", f"R$ {total_despesas:.2f}")
+        st.metric("Saldo", f"R$ {(total_ganhos - total_despesas):.2f}")
 
 if not st.session_state.logado:
     if st.session_state.pagina == "login":
